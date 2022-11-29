d7bcbbd3500662cfd1794a24ce58e2232f0bb631
Ryano0oceros <getryanfallon@gmail.com>
2022-11-29 01:55:05 -0500

    after 26

--- main.go
+++ main.go
@@ -5,9 +5,5 @@ import "fmt"
 func main() {
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	fmt.Println("DELIMIT")
-	remainingDeck.print()
+	fmt.Println(cards.toString())
 }
